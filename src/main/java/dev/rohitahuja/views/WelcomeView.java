be55c503a3ae6fe65c299ae006e5eaf1d70f3c12
Rohit Ahuja <rohit23ahuja@gmail.com>
2025-07-01 13:53:54 +0530

    welcome page

--- src/main/java/dev/rohitahuja/views/WelcomeView.java
+++ src/main/java/dev/rohitahuja/views/WelcomeView.java
@@ -1,7 +1,8 @@
 package dev.rohitahuja.views;
 
-import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -11,18 +12,26 @@ import com.vaadin.flow.router.Route;
 public class WelcomeView extends VerticalLayout {
 
     public WelcomeView() {
-        add(
-                new H1("About Interactions App"),
-                new Html("""
-                        <div>
-                            <p>
-                                The idea behind Interactions App is to provide a single interface or platform that allows you to interact with your Application using natural language.
-                            </p>
-                            <p>
-                                The App aims to solves various use cases such as: chat with your knowledge base, Chat with your database, execute your business logic, etc.
-                            </p>
-                        </div>
-                        """)
+        setWidthFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setAlignItems(Alignment.CENTER);
+        setPadding(true);
+        setSpacing(true);
+
+        H1 title = new H1("Welcome to Interactions App");
+        Paragraph intro = new Paragraph(
+                "The idea behind Interactions App is to provide a single interface or platform that allows you to interact with your Application using natural language. " +
+                        "The App aims to solves various use cases such as: chat with your knowledge base, Chat with your database, execute your business logic, etc."
         );
+
+        // The image path is relative to the public resources folder
+        Image image = new Image("images/Scaled3x.png", "Interactions App");
+
+        image.setMaxWidth("900px");
+        image.setWidthFull();
+        image.getStyle().set("margin-top", "2em");
+
+        add(title, intro, image);
+
     }
 }
